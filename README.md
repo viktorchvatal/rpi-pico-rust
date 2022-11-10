3d9c95e1842b7827447bc7307de2d05e45bc5be9
Viktor Chvatal <viktor@chvatal.name>
2022-11-10 20:13:05 +0100

    Working blinky demo.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Rust blue pill learning demo
+
+My personal walk through learning Rust development on Raspberry Pi Pico
+
+Before switching to Raspberry Pico, I also learned basics of Rust ARM embedded
+development using following platforms:
+ - Programming Blue Pill development board with STM32F103 microcontroller:
+   https://github.com/viktorchvatal/blue-pill-rust
+
+## Userful Resources
+
+ - https://crates.io/crates/rp-pico
+ - https://reltech.substack.com/p/getting-started-with-rust-on-a-raspberry
+
+## Prerequisities
+
+Install `elf2uf2-rs` tool used to flash program directly using the USB
+mass storage interface
+
+```
+cargo install elf2uf2-rs
+```
+
+## Getting Started
+
+The simplest example just transmits a morse code using the onboard LED:
+[demo-blinky](/demo/demo-blinky/src/main.rs)
+
+RP2040 chip contains onboard firmware that allows to flash program in the
+USB mass storage mode. In order to do that, it is needed to:
+
+ - connect the Rpi Pico to the USB while BOOTSEL button is active
+ - mount the RPI-RP2 device
+ - enable the `runner = "elf2uf2-rs -d"` runner in `.cargo/config`
+ - run `cargo run` to flash and run the program
+ - after run command, device is disconnected and the program is executed,
+   to flash another program version, return to the first step and repeat
+
---
+++ demo/demo-blinky/src/main.rs
@@ -0,0 +1,66 @@
+#![no_std]
+#![no_main]
+
+use embedded_hal::digital::v2::OutputPin;
+use rp_pico::{hal::{self, prelude::*, pac, Sio}, entry};
+use cortex_m::delay::Delay;
+
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    let clocks = hal::clocks::init_clocks_and_plls(
+        rp_pico::XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let sio = Sio::new(pac.SIO);
+
+    let pins = rp_pico::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut led = pins.led.into_push_pull_output();
+
+    let message = ".... . .-.. .-.. ---    ";
+
+    // Transmit the message using the onboard LED
+    loop {
+        for char in message.chars() {
+            match char {
+                // Just delay
+                ' ' => delay.delay_ms(500),
+                // Short blink
+                '.' => {
+                    led.set_high().unwrap();
+                    delay.delay_ms(200);
+                    led.set_low().unwrap();
+                },
+                // Long blink
+                '-' => {
+                    led.set_high().unwrap();
+                    delay.delay_ms(700);
+                    led.set_low().unwrap();
+                },
+                _other => {},
+            }
+            delay.delay_ms(300);
+        }
+    }
+}
